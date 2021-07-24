fbafdc59cba89af620d75c94bee603041ba339af
rizqieffendie <87891065+rizqieffendie@users.noreply.github.com>
2021-07-24 15:58:30 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.uasmoop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+    }
+}
